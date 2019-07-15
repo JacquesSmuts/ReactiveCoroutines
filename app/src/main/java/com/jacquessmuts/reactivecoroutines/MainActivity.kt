a69a3a33cd524b21a1b5fc221b72f23fea9b1cd5
Jacques Chester Smuts <chester@jacquessmuts.com>
2019-07-15 21:12:01 +0200

    Simplified with generics

--- app/src/main/java/com/jacquessmuts/reactivecoroutines/MainActivity.kt
+++ app/src/main/java/com/jacquessmuts/reactivecoroutines/MainActivity.kt
@@ -5,15 +5,18 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancelChildren
-import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
@@ -21,20 +24,17 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     private var fabChannel = Channel<Unit>(1)
 
-    private var buttonChannel = BroadcastChannel<Unit>(1)
+    private var buttonChannel = Channel<Unit>(1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            fabChannel.offer(Unit)
-        }
 
-        button.setOnClickListener {
-            buttonChannel.offer(Unit)
-        }
+        fab.bindClick(fabChannel)
+        button.bindClick(buttonChannel)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -56,28 +56,67 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     override fun onStart() {
         super.onStart()
 
-        launch {
-            while (true){
-                fabChannel.receive()
+//        launch {
+//            while (true){
+//                fabChannel.receive()
+//
+//                Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//            }
+//        }
 
-                Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-            }
+        fabChannel.subscribeThrottled(this) {
+            Snackbar.make(fab, "1 sec Delay", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
 
-        launch {
-
-            while (true) {
-                buttonChannel.openSubscription().receive()
-
-                Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-            }
+        buttonChannel.subscribe(this, Dispatchers.Default) {
+            Snackbar.make(fab, "Normal subscribe", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
+
     }
 
     override fun onDestroy() {
         coroutineContext.cancelChildren()
         super.onDestroy()
+    }
+}
+
+fun View.bindClick(channel: Channel<Unit>) {
+    this.setOnClickListener {
+        channel.offer(Unit)
+    }
+}
+
+fun<T> Channel<T>.subscribe (coroutineScope: CoroutineScope, dispatcher: CoroutineDispatcher? = null, action: () -> Unit) {
+
+    if (dispatcher == null) {
+        coroutineScope.launch {
+            while (true) {
+                receive()
+                action()
+            }
+        }
+    } else {
+        coroutineScope.launch(dispatcher) {
+            while (true) {
+                receive()
+                action()
+            }
+        }
+    }
+}
+
+fun<T> Channel<T>.subscribeThrottled (coroutineScope: CoroutineScope, throttleTime: Long = 1000, action: () -> Unit) {
+
+    require(throttleTime >= 0)
+
+    coroutineScope.launch {
+        while (true) {
+            receive()
+            action()
+            delay(throttleTime)
+        }
     }
 }
